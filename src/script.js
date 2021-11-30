7bc40e2e519591903b31e807e2ba4d6090cdd8b0
gjoeyjoe <j.j.schey@hotmail.com>
2021-11-29 17:17:24 -0800

    Creation
    Creation of site.

---
+++ src/script.js
@@ -0,0 +1,29 @@
+var countDownDate = new Date("Dec 7, 2021 00:00:00").getTime();
+var countdownTimer = document.getElementById('countdown-timer')
+// Update the count down every 1 second
+var x = setInterval(function() {
+
+  // Get today's date and time
+  var now = new Date().getTime();
+
+  // Find the distance between now and the count down date
+  var distance = countDownDate - now;
+
+  // Time calculations for days, hours, minutes and seconds
+  var days = Math.floor(distance / (1000 * 60 * 60 * 24));
+  var hours = Math.floor((distance % (1000 * 60 * 60 * 24)) / (1000 * 60 * 60));
+  var minutes = Math.floor((distance % (1000 * 60 * 60)) / (1000 * 60));
+  var seconds = Math.floor((distance % (1000 * 60)) / 1000);
+
+  // Display the result in the element with id="demo"
+  countdownTimer.innerHTML = days + "d " + hours + "h "
+  + minutes + "m " + seconds + "s ";
+
+  // If the count down is finished, write some text
+  if (distance < 0) {
+    clearInterval(x);
+    countdownTimer.innerHTML = "EXPIRED";
+  }
+
+  countdownTimer.style.display = 'block';
+}, 1000);
